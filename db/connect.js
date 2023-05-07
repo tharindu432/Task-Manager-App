e858abfd36e03247580544d45e4bcbd4cc3c7be2
tharindu432 <tharinduchaturanga432@gmail.com>
2023-05-07 07:10:57 +0530

    db setup

--- db/connect.js
+++ db/connect.js
@@ -1,11 +1,9 @@
 const mongoose=require('mongoose');
 
 
-const connectionString=
-'mongodb+srv://tharinduchaturanga432:VkMAguLkb1lr0S9q@cluster0.vol8fmg.mongodb.net/?retryWrites=true&w=majority';
 
 const connectDB=(url)=>{
-return mongoose.connect(connectionString);
+return mongoose.connect(url);
 
 }
 
